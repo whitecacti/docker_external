4aaee008e22dc6d31132ea451c70034707ca9699
whitecacti <storant@gmail.com>
2025-09-01 21:02:18 -0700

    modifying with local address

--- 0EXT_searxng/move_build.sh
+++ 0EXT_searxng/move_build.sh
@@ -1,10 +1,10 @@
-ssh netcup "\
+ssh nc2 "\
         mkdir -p ~/docker/searxng; \
         mkdir -p ~/docker/searxng/config; \
         "
 
-scp -r * netcup:~/docker/searxng/
+scp -r * nc2:~/docker/searxng/
 
-ssh netcup "cd ~/docker/searxng/; \
+ssh nc2 "cd ~/docker/searxng/; \
             docker compose --env-file ./config/.env down; \
             docker compose --env-file ./config/.env up -d"
