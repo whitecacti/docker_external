c28235cbc09881f177c4205829f4485a469ca7b2
whitecacti <github.unearth399@8alias.com>
2025-09-14 19:34:13 -0700

    network and ha updates

--- 110_syncthing/move_build.sh
+++
@@ -1,9 +0,0 @@
-ssh docker_host_110 "mkdir -p ~/docker; \
-                    mkdir -p ~/docker/syncthing; \
-                    mkdir -p ~/docker/syncthing/config; \
-                    "
-scp docker-compose.yaml docker_host_110:~/docker/syncthing/docker-compose.yaml
-scp ./config/.env docker_host_110:~/docker/syncthing/config/.env
-ssh docker_host_110 "cd ~/docker/syncthing; \
-                    docker compose --env-file ./config/.env down; \
-                    docker compose --env-file ./config/.env up -d"
